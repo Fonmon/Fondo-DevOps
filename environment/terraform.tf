24b4dc835c4cf59aacec7b09596bdea454553bdc
Miguel Montanez <cmiguelmg@gmail.com>
2021-08-18 09:04:23 -0500

    leave ubuntu image static in terraform

--- environment/terraform.tf
+++ environment/terraform.tf
@@ -92,7 +92,7 @@ data "aws_ami" "latest_ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-20190617"]
   }
   filter {
     name   = "virtualization-type"
